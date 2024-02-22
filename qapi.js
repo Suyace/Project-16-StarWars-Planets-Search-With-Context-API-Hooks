f54e3c1e598ce9fc65c77fb4b2e056d13f0cafaa
Suyace <160857162+Suyace@users.noreply.github.com>
2024-02-22 17:53:09 +0530

    Changes

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"65d73bc90072400020ea678d",
+				"65d73bc90072400020ea6790"
+			]
+		},
+		"65d73bc90072400020ea678d": {
+			"id": "65d73bc90072400020ea678d",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"65d73bc90072400020ea6790": {
+			"id": "65d73bc90072400020ea6790",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
